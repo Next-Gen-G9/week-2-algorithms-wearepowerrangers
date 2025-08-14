797bbc62b93f643e38d98eb10fe9692e97aed08d
mengmeng-sys <na634997@gmail.com>
2025-08-14 11:34:40 +0700

    new

--- utils/LibraryUtils.cpp
+++ utils/LibraryUtils.cpp
@@ -55,12 +55,29 @@ std::vector<Book> loadBooks() {
 // This exercise covers: functions, std::vector, std::string
 void addBook(std::vector<Book>& books) {
     // 1. Create a new 'Book' object.
+    Book newBook;
+                 
     // 2. Set its 'id'. A simple way is to use the current size of the vector: books.size() + 1.
+    newBook.id = books.size() + 1;
+      
     // 3. Prompt the user to "Enter title: " and read the input into the book's 'title'.
+    std::cout << "Enter title: ";
+    std::getline(std::cin, newBook.title);
+                        
     // 4. Prompt the user to "Enter author: " and read the input into the book's 'author'.
+    std::cout << "Enter author: ";
+    std::getline(std::cin, newBook.author);
+
+
+
     // 5. Set the book's 'isAvailable' status to true.
+    newBook.isAvailable = true;
+        
     // 6. Add the new book object to the 'books' vector using push_back().
+    books.push_back(newBook);
+                
     // 7. Print a confirmation message.
+    std::cout << "Book added successfully! ID: " << newBook.id << std::endl;
 }
 
 // STUDENT TASK: Implement the displayAllBooks function.
